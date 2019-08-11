df3f090a276897a3e01a48ce6bf332e6ba161f9b
Shawn N Rebello <shawn.nigel@gmail.com>
2019-08-11 18:21:44 +0530

    Added API group name & version

---
+++ pkg/api/register.go
@@ -0,0 +1,29 @@
+package v1
+
+import (
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+)
+
+var SchemeGroupVersion = schema.GroupVersion{
+	Group:   "shawnrebello.com",
+	Version: "v1",
+}
+
+var (
+	SchemeBuilder      runtime.SchemeBuilder
+	localSchemeBuilder = &SchemeBuilder
+	AddToScheme        = localSchemeBuilder.AddToScheme
+)
+
+func init() {
+	localSchemeBuilder.Register(addKnownTypes)
+}
+
+func addKnownTypes(scheme *runtime.Scheme) error {
+	return nil
+}
+
+func Resource(resource string) schema.GroupResource {
+	return SchemeGroupVersion.WithResource(resource).GroupResource()
+}
