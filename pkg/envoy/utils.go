f244608a3d97221de0bcdc7f03d65678a939be54
Shawn N Rebello <shawn.nigel@gmail.com>
2019-08-17 00:49:20 +0530

    Generating envoy bootstrap config

--- pkg/envoy/utils.go
+++ pkg/envoy/utils.go
@@ -1,6 +1,9 @@
 package envoy
 
 import (
+	"encoding/json"
+	"log"
+
 	appsv1 "k8s.io/api/apps/v1"
 	apiv1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -9,6 +12,8 @@ import (
 	v1 "github.com/starizard/kube-envoy-controller/pkg/api/example.com/v1"
 	client "github.com/starizard/kube-envoy-controller/pkg/client/clientset/versioned"
 )
+
+var apiType = "GRPC"
 
 //Deployment returns a spec for an envoy deployment
 func Deployment(envoy *v1.Envoy) *appsv1.Deployment {
@@ -92,6 +97,125 @@ func Service(envoy *v1.Envoy) *apiv1.Service {
 		},
 	}
 	return service
+}
+
+func addAdminConfig() Admin {
+	return Admin{
+		AccessLogPath: "/dev/stderr",
+		Address: Address{
+			SocketAddress: SocketAddress{
+				Address:   "127.0.0.1",
+				PortValue: 15000,
+			},
+		},
+	}
+}
+
+func addLDSConfig(envoy *v1.Envoy) LdsConfig {
+	clusterName := envoy.Spec.XDS.Name
+	return LdsConfig{
+		APIConfigSource: APIConfigSource{
+			APIType: apiType,
+			GrpcServices: GrpcServices{
+				Grpc: Grpc{
+					ClusterName: clusterName,
+				},
+			},
+		},
+	}
+}
+
+func addCDSConfig(envoy *v1.Envoy) CdsConfig {
+	clusterName := envoy.Spec.XDS.Name
+	return CdsConfig{
+		APIConfigSource: APIConfigSource{
+			APIType: apiType,
+			GrpcServices: GrpcServices{
+				Grpc: Grpc{
+					ClusterName: clusterName,
+				},
+			},
+		},
+	}
+}
+
+func addADSConfig(envoy *v1.Envoy) AdsConfig {
+	clusterName := envoy.Spec.XDS.Name
+	return AdsConfig{
+		APIType: apiType,
+		GrpcServices: GrpcServices{
+			Grpc: Grpc{
+				ClusterName: clusterName,
+			},
+		},
+	}
+}
+
+func addDynamicResources(envoy *v1.Envoy) DynamicResources {
+	return DynamicResources{
+		AdsConfig: addADSConfig(envoy),
+		CdsConfig: addCDSConfig(envoy),
+		LdsConfig: addLDSConfig(envoy),
+	}
+}
+
+func addStaticResources(envoy *v1.Envoy) StaticResources {
+	clusterName := envoy.Spec.XDS.Name
+	xdsHostname := envoy.Spec.XDS.Host
+	xdsPort := envoy.Spec.XDS.Port
+	return StaticResources{
+		Clusters: []Clusters{{
+			Name:           clusterName,
+			ConnectTimeout: "5s",
+			Type:           "STRICT_DNS",
+			Hosts: []Hosts{{
+				SocketAddress: SocketAddress{
+					Address:   xdsHostname,
+					PortValue: xdsPort,
+				},
+			},
+			},
+		},
+		},
+	}
+}
+
+func makeEnvoyConfig(envoy *v1.Envoy) *Bootstrap {
+	envoyconfig := &Bootstrap{
+		Node: Node{
+			Cluster: "service_1",
+			ID:      "test-id",
+		},
+
+		StaticResources:  addStaticResources(envoy),
+		DynamicResources: addDynamicResources(envoy),
+		Admin:            addAdminConfig(),
+	}
+	return envoyconfig
+}
+
+//ConfigMap returns a spec for an envoy bootstrap config
+func ConfigMap(envoy *v1.Envoy) *apiv1.ConfigMap {
+	var cfgData string
+	conf := makeEnvoyConfig(envoy)
+	jsonString, err := json.Marshal(conf)
+	if err != nil {
+		log.Println(err)
+	}
+
+	cfgData = string(jsonString)
+	log.Println(cfgData)
+
+	data := map[string]string{
+		"envoy.yaml": cfgData,
+	}
+	cfgMap := &apiv1.ConfigMap{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: envoy.Spec.ConfigMapName,
+		},
+		Data: data,
+	}
+	return cfgMap
 }
 
 //UpdateStatus updates the status of an envoy resource
